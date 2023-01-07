5c7deb83604ab6452c8e3d1fc6c96ace3f364972
CuongNguyen <Cuongn1912@gamil.com>
2023-01-07 12:53:22 +0700

    update feature

--- src/pages/Admin/User/Create/UserCreate.js
+++ src/pages/Admin/User/Create/UserCreate.js
@@ -42,50 +42,50 @@ export default function UserCreate(props) {
     return (
         <div className='mx-40'>
             <h2 className='text-center text-xl'>Tạo Tài Khoản</h2>
-            <form onSubmit={formik.handleSubmit}>
-                {/* Email */}
+            <form onSubmit={ formik.handleSubmit }>
+                {/* Email */ }
                 <div className="mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                    <input name="email" value={formik.values.email} onChange={formik.handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" />
-                    {formik.errors.email && formik.touched.email && (
-                        <p className='m-0 mt-1 text-red-600'>{formik.errors.email}</p>
-                    )}
+                    <input name="email" value={ formik.values.email } onChange={ formik.handleChange } className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" />
+                    { formik.errors.email && formik.touched.email && (
+                        <p className='m-0 mt-1 text-red-600'>{ formik.errors.email }</p>
+                    ) }
                 </div>
-                {/* PassWord */}
+                {/* PassWord */ }
                 <div className="mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mật Khẩu</label>
-                    <input type='password' value={formik.values.password} onChange={formik.handleChange} name="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
-                    {formik.errors.password && formik.touched.password && (
-                        <p className='m-0 mt-1 text-red-600'>{formik.errors.password}</p>
-                    )}
+                    <input type='password' value={ formik.values.password } onChange={ formik.handleChange } name="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                    { formik.errors.password && formik.touched.password && (
+                        <p className='m-0 mt-1 text-red-600'>{ formik.errors.password }</p>
+                    ) }
                 </div>
-                {/* UserName */}
+                {/* UserName */ }
                 <div className="mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Họ Tên</label>
-                    <input name="userName" value={formik.values.userName} onChange={formik.handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
-                    {formik.errors.userName && formik.touched.userName && (
-                        <p className='m-0 mt-1 text-red-600'>{formik.errors.userName}</p>
-                    )}
+                    <input name="userName" value={ formik.values.userName } onChange={ formik.handleChange } className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                    { formik.errors.userName && formik.touched.userName && (
+                        <p className='m-0 mt-1 text-red-600'>{ formik.errors.userName }</p>
+                    ) }
                 </div>
-                {/* Phone */}
+                {/* Phone */ }
                 <div className="mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Số Điện Thoại</label>
-                    <input name="phoneNumber" value={formik.values.phoneNumber} onChange={formik.handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
-                    {formik.errors.phoneNumber && formik.touched.phoneNumber && (
-                        <p className='m-0 mt-1 text-red-600'>{formik.errors.phoneNumber}</p>
-                    )}
+                    <input name="phoneNumber" value={ formik.values.phoneNumber } onChange={ formik.handleChange } className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                    { formik.errors.phoneNumber && formik.touched.phoneNumber && (
+                        <p className='m-0 mt-1 text-red-600'>{ formik.errors.phoneNumber }</p>
+                    ) }
                 </div>
-                {/* Type User */}
+                {/* Type User */ }
                 <div className="mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Loại Người Dùng</label>
-                    <select name="typeUser" value={formik.values.typeUser} onChange={formik.handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        {lstType.map((type, index) => {
-                            return <option key={index} value={type.id}>{type.nameType}</option>
-                        })}
+                    <select name="typeUser" value={ formik.values.typeUser } onChange={ formik.handleChange } className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        { lstType.map((type, index) => {
+                            return <option key={ index } value={ type.id }>{ type.nameType }</option>
+                        }) }
                     </select>
                 </div>
                 <div className='flex justify-center'>
-                    <button type="submit" className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tạo Mới</button>
+                    <button type="submit" className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Đăng kí</button>
                 </div>
             </form>
         </div>
